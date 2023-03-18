771da8d4be54b1b542a3d2ce7c7c7ddcf823daff
Flash-Z <65103912+Flash-Z@users.noreply.github.com>
2023-03-19 00:33:11 +0800

    UI update

--- ToDoList/todolist/UI.py
+++ ToDoList/todolist/UI.py
@@ -15,7 +15,7 @@ help_body = {
     f"§b{prefix} add <name> (<detail>) (<progress>)": "§r添加/修改<name>项目，可选参数为描述与进度",
 }
 
-def get_list():
+def get_list(target_tag=""):
     c = ['']
 
     list_head = RText(f'================== §bToDoList §r==================').c(
@@ -27,10 +27,18 @@ def get_list():
         for tag in fun.list_dic.get(name)["tags"]:
             if tag not in tag_list and tag != "defult":
                 tag_list += f'{tag} '
+                if tag == target_tag or target_tag =="":
+                    include = True
+        if not include:
+            continue
         list_msg = RTextList(
                             f'\n- ',
                             RText(f'[×] ', color = RColor.red).c(RAction.suggest_command, f'!!td del {name}')
                                 .h(RText(f'Delete', color = RColor.red)),
+                            RText(f'[T+] ', color = RColor.green).c(RAction.suggest_command, f'!!td tag add {name} ')
+                                .h(RText(f'Add tag', color = RColor.green)),
+                                RText(f'[T-] ', color = RColor.green).c(RAction.suggest_command, f'!!td tag del {name} ')
+                                .h(RText(f'Delete a tag', color = RColor.red)),
                             RText(f'§b{name}').c(RAction.suggest_command, 
                                                 f'!!td add {name} {fun.list_dic.get(name)["detail"][0]} {fun.list_dic.get(name)["progress"]}')
                             .h(
@@ -59,27 +67,27 @@ def get_tags():
         tag_list.append((RText(f'§b<{ctag}> ').c(RAction.suggest_command,f'!!td tag {ctag}'))).h(f'单击以进入对应tag列表')
     return tag_list
     
-def get_taged_list(target):
-    c = ['']
-    list_head = RText(f'================== §b{target} §r==================').c(
-        RAction.suggest_command, f'!!td tag').h(f'§b!!td tag')
-    c.append(list_head)
-    for name, list_info in fun.list_dic.items():
-        for tag in fun.list_dic.get(name)["tags"]:
-            if tag == target:
-                list_msg = RTextList(
-                                    f'\n- ',
-                                    RText(f'[×] ', color = RColor.red).c(RAction.suggest_command, f'!!td del {name}')
-                                        .h(RText(f'Delete', color = RColor.red)),
-                                    RText(f'§b{name}').c(RAction.suggest_command, 
-                                                        f'!!td add {name} {fun.list_dic.get(name)["detail"][0]} {fun.list_dic.get(name)["progress"]}')
-                                    .h(
-                                        f'§r点击项目名以修改信息\n'
-                                        f'§7最后修改者:§6 {fun.list_dic.get(name)["creator"]} §7时间:§6 {fun.list_dic.get(name)["time"]}',
-                                        f'\n§7项目描述:§6 {fun.list_dic.get(name)["detail"][0]}' if fun.list_dic.get(name)["detail"][0] !="" else "",
-                                        f'\n§7进度描述:§6 {fun.list_dic.get(name)["progress"]}' if fun.list_dic.get(name)["progress"] !="" else ""
-                                    )
-                                )
-                c.append(list_msg)
-                break
-    return c
+# def get_taged_list(target):
+#     c = ['']
+#     list_head = RText(f'================== §b{target} §r==================').c(
+#         RAction.suggest_command, f'!!td tag').h(f'§b!!td tag')
+#     c.append(list_head)
+#     for name, list_info in fun.list_dic.items():
+#         for tag in fun.list_dic.get(name)["tags"]:
+#             if tag == target:
+#                 list_msg = RTextList(
+#                                     f'\n- ',
+#                                     RText(f'[×] ', color = RColor.red).c(RAction.suggest_command, f'!!td del {name}')
+#                                         .h(RText(f'Delete', color = RColor.red)),
+#                                     RText(f'§b{name}').c(RAction.suggest_command, 
+#                                                         f'!!td add {name} {fun.list_dic.get(name)["detail"][0]} {fun.list_dic.get(name)["progress"]}')
+#                                     .h(
+#                                         f'§r点击项目名以修改信息\n'
+#                                         f'§7最后修改者:§6 {fun.list_dic.get(name)["creator"]} §7时间:§6 {fun.list_dic.get(name)["time"]}',
+#                                         f'\n§7项目描述:§6 {fun.list_dic.get(name)["detail"][0]}' if fun.list_dic.get(name)["detail"][0] !="" else "",
+#                                         f'\n§7进度描述:§6 {fun.list_dic.get(name)["progress"]}' if fun.list_dic.get(name)["progress"] !="" else ""
+#                                     )
+#                                 )
+#                 c.append(list_msg)
+#                 break
+#     return c
--- ToDoList/todolist/operate_list.py
+++ ToDoList/todolist/operate_list.py
@@ -35,7 +35,7 @@ def operate_list(server, info, args):
         if len(args) == 2: # 返回所有tag列表
             server.reply(info, get_tags())
         elif len(args) == 3: # 返回相应tag列表
-            server.reply(info, RTextList(*get_taged_list(args[2])))
+            server.reply(info, RTextList(*get_list(args[2])))
         elif len(args) == 5: 
             if args[2] == "add" or args[2] == "a": # !!td tag add <name> <tag>
                 if args[4] not in fun.list_dic[args[3]]:
@@ -43,14 +43,22 @@ def operate_list(server, info, args):
                     fun.save()
                     server.reply(info, f'§b[ToDoList]§a已更新ToDo {args[2]} 的 tag {args[4]}')
             if args[2] == "del" or args[2] == "d": # !!td tag del <name> <tag>
-                new_tag = []
-                temp_tag = fun.list_dic[args[3]]["tags"]
-                for old_tag in temp_tag:
-                    if old_tag != args[4]:
-                        new_tag.append(old_tag)
-                fun.list_dic[args[3]]["tags"] = new_tag
-                fun.save()
-                server.reply(info, f'§b[ToDoList]§a已删除ToDo {args[2]} 的 tag {args[4]}')
+                if args[4] != "defult":
+                    new_tag = []
+                    temp_tag = fun.list_dic[args[3]]["tags"]
+                    for old_tag in temp_tag:
+                        if old_tag != args[4]:
+                            new_tag.append(old_tag)
+                        else:
+                            found_oldtag = True
+                    if found_oldtag:
+                        fun.list_dic[args[3]]["tags"] = new_tag
+                        fun.save()
+                        server.reply(info, f'§b[ToDoList]§a已删除ToDo {args[2]} 的 tag {args[4]}')
+                    else:
+                        server.reply(info, f'§b[ToDoList]§4ToDo {args[2]} 没有 tag {args[4]}')
+                else:
+                    server.reply(info, f'§b[ToDoList]§4不能删除 defult tag')
             
             
     elif len(args) == 2:
